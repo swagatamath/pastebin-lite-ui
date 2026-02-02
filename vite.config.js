fc33dfb13cf29f389b50f1821127b7bc2acfd21c
schakravarty <schakravarty@madisonlogic.com>
2026-02-02 12:51:42 +0530

    made dynamic changes

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://pastebin-backend-new.vercel.app',
+        changeOrigin: true,
+        secure: true,
+      },
+      '/p': {
+        target: 'https://pastebin-backend-new.vercel.app',
+        changeOrigin: true,
+        secure: true,
+      }
+    }
+  }
 })
